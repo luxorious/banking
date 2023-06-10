0c69e43ebf986013894952ae65eaad7a4a1c7e0c
luxorious <vovan_194@mail.ru>
2023-06-10 19:19:35 +0200

    first creation.

--- src/main/java/com/banking/repository/AccountRepository.java
+++ src/main/java/com/banking/repository/AccountRepository.java
@@ -5,12 +5,14 @@ import com.banking.entity.entityEnumerations.AccountStatus;
 import com.banking.entity.entityEnumerations.AccountType;
 import com.banking.entity.entityEnumerations.CurrencyCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
 
     Optional<Account> findAccountById(UUID uuid);
--- src/main/java/com/banking/repository/ClientRepository.java
+++
@@ -1,4 +0,0 @@
-package com.banking.repository;
-
-public interface ClientRepository {
-}
