68da218a86eb3842ab6eb7fa61c947c5b9151926
luxorious <vovan_194@mail.ru>
2023-07-15 13:52:47 +0200

    changes in security

--- src/main/java/com/banking/security/SecurityConfig.java
+++ src/main/java/com/banking/security/SecurityConfig.java
@@ -16,6 +16,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -24,10 +27,10 @@ public class SecurityConfig extends WebSecurityConfiguration {
     private final UserDetailsService userDetailsService;
     private final FileUtil fileUtil;
 
-    @Value("${security.userPath}")
-    private String userPath;
-    @Value("${security.managerPath}")
-    private String managerPath;
+    @Value("${security.userLinks}")
+    private String userLinks;
+    @Value("${security.managerLinks}")
+    private String managerLinks;
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
@@ -36,13 +39,21 @@ public class SecurityConfig extends WebSecurityConfiguration {
 
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers(fileUtil.writeFromFileToList(userPath).toArray(new String[0])).hasRole("MANAGER")
-                        .requestMatchers(fileUtil.writeFromFileToList(managerPath).toArray(new String[0])).hasRole("USER")
+                        .requestMatchers(convertStringToList(userLinks).toArray(
+                                new String[0])).hasRole(String.valueOf(Role.USER))
+                        .requestMatchers(convertStringToList(managerLinks).toArray(
+                                new String[0])).hasRole(String.valueOf(Role.MANAGER))
                         .anyRequest().hasRole(String.valueOf(Role.ADMINISTRATOR))
                 )
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
+
+    public List<String> convertStringToList(String input) {
+        String[] values = input.split(",\\s*");
+        return Arrays.asList(values);
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
