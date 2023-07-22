d1a81bceb73e51c20a95e9d1fd322730ec4d9dd2
luxorious <vovan_194@mail.ru>
2023-07-22 14:47:36 +0200

    create Javadoc

--- src/main/java/com/banking/service/implementation/DocumentServiceImpl.java
+++ src/main/java/com/banking/service/implementation/DocumentServiceImpl.java
@@ -9,9 +9,7 @@ import com.banking.service.interfaces.utility.GetEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.UUID;
 
 /**
@@ -63,17 +61,6 @@ public class DocumentServiceImpl implements DocumentService {
     }
 
     /**
-     * Finds the document with the specified ID.
-     *
-     * @param id The ID of the document to find.
-     * @return The document with the specified ID.
-     */
-    @Override
-    public Document findDocumentById(Integer id) {
-        return getDocument.getEntity(documentRepository.findDocumentById(id));
-    }
-
-    /**
      * Edits the document associated with the specified client.
      *
      * @param clientId   The UUID of the client to edit the document for.
@@ -93,27 +80,5 @@ public class DocumentServiceImpl implements DocumentService {
             log.error("something went wrong");
             throw new BadAccountDataException("Error");
         }
-    }
-
-    /**
-     * Edits the image of the document with the specified ID.
-     *
-     * @param id         The ID of the document to edit the image for.
-     * @param image      The new image data as a MultipartFile.
-     * @param isPassport A boolean flag indicating whether the image is for the passport or registration.
-     * @return The updated Document object.
-     * @throws IOException if there was an error reading the image data.
-     */
-    @Override
-    public Document editImageById(Integer id, MultipartFile image, boolean isPassport) throws IOException {
-        byte[] imgBytes = image.getBytes();
-        Document document = getDocument.getEntity(documentRepository.findDocumentById(id));
-        if (isPassport) {
-            document.setPassport(imgBytes);
-        } else {
-            document.setRegistration(imgBytes);
-        }
-        documentRepository.save(document);
-        return document;
     }
 }
